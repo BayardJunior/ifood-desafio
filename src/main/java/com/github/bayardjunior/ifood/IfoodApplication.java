ae4a24de965acbce91aed6e3b1cbd6324c540714
Bayard Junior <sobreira.b@gmail.com>
2023-01-19 21:12:15 -0300

    first commit

---
+++ src/main/java/com/github/bayardjunior/ifood/IfoodApplication.java
@@ -0,0 +1,13 @@
+package com.github.bayardjunior.ifood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/bayardjunior/ifood/IfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.bayardjunior.ifood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
